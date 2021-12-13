49f9c47f416811990d219e3a9f95478d7e96c6f4
Desislava Kracheva <desislava.92@abv.bg>
2021-12-13 21:34:01 +0200

    make change

--- arrayManipulations2.js
+++ arrayManipulations2.js
@@ -1,34 +1,35 @@
-function solveForSecondTime(arr) {
-    let numbers = arr.shift().split(' ').map(Number);
-   
+function solve(arr){
+    let arrFromNumbers = arr.shift().split(' ').map(Number);
+    
+    
     for(let i = 0; i< arr.length; i++){
-    let [command, firstArg, secondArg] = arr.shift();
-
-    switch (command) {
-        case 'Add':numbers.push(firstArg);
-            break;
-        case 'Remove':
-            let i = numbers.indexOf(firstArg);
-            numbers = numbers.splice(i,1);
-            break;
-        case 'RemoveAt':
-            numbers= numbers.splice(firstArg,1)
-            break;
-        case 'Insert':numbers = numbers.splice(firstArg,1,secondArg)
-            break;
-
-
     
+    let token = arr[i].split(' ');
+    
+    switch(token[0]){
+    case'Add':
+    
+        arrFromNumbers.push(token[1]); break;
+    case 'Remove':
+     
+        arrFromNumbers = arrFromNumbers.filter(el => el!== token[1]); break;
+    
+      
+    case 'RemoveAt':
+        arrFromNumbers.splice(token[1],1);break;
+    case 'Insert': arrFromNumbers.splice(token[2],0 , token[1]);break;
     }
-
-}
     
-console.log(numbers.join(' '));
-}
-
-solveForSecondTime(['4 19 2 53 6 43',
+    
+        
+    }
+    
+    console.log(arrFromNumbers.join(' '))
+    }
+    
+    solve(['4 19 2 53 6 43',
     'Add 3',
     'Remove 2',
     'RemoveAt 1',
     'Insert 8 3']
-)
+    )
